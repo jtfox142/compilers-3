4e78dbadcf1333e3ddf6a203444e4d75b403955f
Jacob (J.T.) Fox <jtfox142@gmail.com>
2024-04-10 20:19:48 -0500

    Removing unnecessary output

--- statSem.cpp
+++ statSem.cpp
@@ -27,37 +27,37 @@ void statSem::driver(node::Node *tree) {
 //root, left, middle, right
 void traversePreorder(node::Node *root, symbolTable::Scope *scope) {
     if(root == NULL) return;
-    std::cout << "Current Token: " << root->getData().tokenInstance << std::endl;
+    //std::cout << "Current Token: " << root->getData().tokenInstance << std::endl;
 
     if(root->getData().tokenInstance == "block()") {
-        std::cout << "Calling block()\n";
+        //std::cout << "Calling block()\n";
         //Create a new scope to operate in
         processBlock(root);
         return;
     }
     else if(root->getData().tokenInstance == "vars()") {
         //Add the identifiers to the stack
-        std::cout << "Calling vars()\n";
+        //std::cout << "Calling vars()\n";
         processVars(root, scope);
         return;
     }
     else if(root->getData().tokenInstance == "func()") { //<func> -> func Identifier <block>
         //skip processing func identifier
-        std::cout << "Calling func()\n";
+        //std::cout << "Calling func()\n";
         traversePreorder(root->getChildThree(), scope);
         return;
     }
     else if(root->getData().tokenInstance == "goto()") { //<goto> -> jump Identifier
         //skip processing function identifier, because I'm only supposed to process vars
-        std::cout << "Skipping goto()\n";
+        //std::cout << "Skipping goto()\n";
         return;
     }
     else if(root->getData().tokenId == token::idTok) {
-        std::cout << "Line " << root->getData().lineNumber << ": calling processIdentifier() for " << root->getData().tokenInstance << std::endl;
+        //std::cout << "Line " << root->getData().lineNumber << ": calling processIdentifier() for " << root->getData().tokenInstance << std::endl;
         processIdentifier(root, scope);
     }
 
-    std::cout << "Traversing\n";
+    //std::cout << "Traversing\n";
 
     traversePreorder(root->getChildOne(), scope);
     traversePreorder(root->getChildTwo(), scope);
@@ -72,7 +72,7 @@ void traversePreorder(node::Node *root, symbolTable::Scope *scope) {
 //Starts a local scope for new identifiers within the block
 void processBlock(node::Node *root) {
     symbolTable::Scope *scope = new symbolTable::Scope();
-    std::cout << "Entering a new block scope.\n";
+    //std::cout << "Entering a new block scope.\n";
 
     //<block> -> { <vars> <stats> }
     //Traverse the block until the block is empty
@@ -85,7 +85,7 @@ void processBlock(node::Node *root) {
     while(scope->getVarCount() > 0) {
         scope->pop();
     }
-    std::cout << "Exiting block scope.\n";
+    //std::cout << "Exiting block scope.\n";
 }
 
 
@@ -93,21 +93,19 @@ void processVars(node::Node *root, symbolTable::Scope *local) {
     //<vars> -> empty | create Identifier ; | create Identifier := Integer ; <vars>
     if(root == NULL || root->getChildOne() == NULL) return;
 
-    std::cout<< "Identifier: " << root->getChildTwo()->getData().tokenInstance << std::endl;
-    std::cout<< "root->getChildOne(): " << root->getChildOne()->getData().tokenInstance << std::endl;
+    //std::cout<< "Identifier: " << root->getChildTwo()->getData().tokenInstance << std::endl;
     //If vars() subtree is not empty, push the identifier onto the stack
     if(root->getChildOne()->getData().tokenInstance == "create") {
         token::Token token = root->getChildTwo()->getData();
 
-        std::cout << "segFault in local->find.\n";
         //Ensure the identifier hasn't been used before
         if(local->find(token) == -1 && symbolTable::verify(token) == false) {
-            std::cout << "Pushing token " << token.tokenInstance << " onto the stack.\n";
+            //std::cout << "Pushing token " << token.tokenInstance << " onto the stack.\n";
             local->push(token);
-            std::cout << "Token pushed successfully\n";
+            //std::cout << "Token pushed successfully\n";
         }
         else {
-            std::cerr << "ERROR Line " << token.lineNumber << ": Identifier " << token.tokenInstance << " is declared multiple times.";
+            std::cerr << "ERROR Line " << token.lineNumber << ": Identifier " << token.tokenInstance << " is declared multiple times.\n";
             exit(1);
         }
     }
@@ -127,7 +125,7 @@ void processIdentifier(node::Node *root, symbolTable::Scope *local) {
             exit(1);
         }
     }
-    else
-        std::cout << "Identifier " << root->getData().tokenInstance << " is valid in this scope.\n";
+    /*else
+        std::cout << "Identifier " << root->getData().tokenInstance << " is valid in this scope.\n";*/
 
 }
--- symbolTable.cpp
+++ symbolTable.cpp
@@ -23,48 +23,29 @@ bool symbolTable::verify(token::Token token) {
 
 //Pushes a token onto TOS
 void symbolTable::Scope::push(token::Token token) {
-    std::cout << "abt to push\n";
     symbolTable::stack.push_back(token);
-    std::cout << symbolTable::stack.back().tokenInstance << " has been pushed to the stack.\n";
+    //std::cout << symbolTable::stack.back().tokenInstance << " has been pushed to the stack.\n";
     varCount++;
 }
 
 //Removes a token from TOS
 void symbolTable::Scope::pop() {
-    std::cout << "Popping " << symbolTable::stack.back().tokenInstance << " from the stack.\n";
+    //std::cout << "Popping " << symbolTable::stack.back().tokenInstance << " from the stack.\n";
     symbolTable::stack.pop_back();
     varCount--;
 }
 
 //Returns distance from TOS if string identifier is found, returns -1 otherwise
 int symbolTable::Scope::find(token::Token tokenInstance) {
-    std::cout << "Do I make it into find()?\n";
     std::vector<token::Token>::reverse_iterator it;
     int stackCounter = 0;
-
-
-    //Maybe prevents segFault
-    /*it = symbolTable::stack.rend();
-
-    if(it == symbolTable::stack.rbegin()) {
-        std::cout << "stack end == stack begin\n";
-        return -1;
-    }
-
-    std::string stopsegfault = it->tokenInstance;
-    std::cout << "Token instance: " << stopsegfault << std::endl;*/
-    std::cout << "About to enter for loop in find()\n";
     
     for(it = symbolTable::stack.rbegin(); it != symbolTable::stack.rend(); it++) {
-        std::cout << "inside of for loop\n";
         if(it->tokenInstance == tokenInstance.tokenInstance) {
-            std::cout << "returning stackCounter\n";
             return stackCounter;
         }
-        std::cout << "incrementing stackCounter\n";
         stackCounter++;
     }
 
-    std::cout << "returning -1\n";
     return -1;
 }
